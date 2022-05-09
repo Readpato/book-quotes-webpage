a95c3a70d359723c80307d1ecd2d0747e0d0c865
Patrick <patoread@gmail.com>
2022-05-09 17:30:43 +0200

    style: Add style for form
    The form where the quotes are going to be added to the main project has been styled

--- pages/add-quote.vue
+++ pages/add-quote.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>This is the page to add quotes!</h1>
+  <section class="w-4/5 mx-auto h-auto p-3 mt-5">
+    <h1 class="text-center text-xl">Add a quote</h1>
+    <form class="flex flex-col space-y-2 w-full md:max-w-lg mx-auto">
+      <div class="flex flex-col space-y-1">
+        <label for="author-name">Author's Name</label>
+        <input
+          type="text"
+          id="author-name"
+          class="border-2 border-red-400 rounded p-1 focus:outline-none focus:ring-2 focus:ring-red-500"
+        />
+      </div>
+      <div class="flex flex-col space-y-1">
+        <label for="book-name">Book Name</label>
+        <input
+          type="text"
+          id="book-name"
+          class="border-2 border-red-400 rounded p-1 focus:outline-none focus:ring-2 focus:ring-red-500"
+        />
+      </div>
+      <div class="flex flex-col space-y-1">
+        <label for="quote-text">Quote</label>
+        <textarea
+          id="book-name"
+          class="border-2 border-red-400 rounded p-1 focus:outline-none focus:ring-2 focus:ring-red-500"
+        />
+      </div>
+      <button
+        @click.prevent=""
+        class="border-2 border-cyan-400 w-40 p-3 mx-auto rounded bg-cyan-400 text-white text-base hover:border-cyan-300"
+      >
+        Submit Quote
+      </button>
+    </form>
+  </section>
 </template>

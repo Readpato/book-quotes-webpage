7a2471f7b5dbe62eb63337f7d66ffe9439340e82
Patrick <patoread@gmail.com>
2022-05-09 17:31:47 +0200

    style: Fix changing height
    The nav bar has been fixed to not change height once it changes from mobile to tablet sizes

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="w-full h-16 md:h-14 bg-red-500 flex justify-between items-center p-3 text-white"
+    class="w-full h-16 bg-red-500 flex justify-between items-center p-3 text-white"
   >
     <h1 class="text-white text-base md:text-xl">Book Quotes</h1>
     <ul class="flex space-x-3 text-sm md:text-base">

d5d87ea3142111f0a422edd1fe40d65c76a500c6
Patrick <patoread@gmail.com>
2022-05-09 16:04:13 +0200

    refactor: Make navigation bar responsive
    The navigation bar has been modified to be responsive at all different screen resolutions

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,18 +1,18 @@
 <template>
   <nav
-    class="w-full h-14 bg-red-500 flex justify-between items-center p-3 text-white"
+    class="w-full h-16 md:h-14 bg-red-500 flex justify-between items-center p-3 text-white"
   >
-    <h1 class="text-white text-xl">Book Quotes</h1>
-    <ul class="flex space-x-3">
+    <h1 class="text-white text-base md:text-xl">Book Quotes</h1>
+    <ul class="flex space-x-3 text-sm md:text-base">
       <li
-        class="cursor-pointer hover:outline hover:outline-red-400 p-1 rounded"
+        class="cursor-pointew-screenr hover:outline hover:outline-red-400 p-1 rounded"
       >
         <NuxtLink to="/">Quotes</NuxtLink>
       </li>
       <li
         class="cursor-pointer hover:outline hover:outline-red-400 p-1 rounded"
       >
-        <NuxtLink to="/add-quote">Add Quote</NuxtLink>
+        <NuxtLink to="/add-quote">Add</NuxtLink>
       </li>
       <li
         class="cursor-pointer hover:outline hover:outline-red-400 p-1 rounded"

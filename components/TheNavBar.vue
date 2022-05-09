71ea57449394da58fa4d7b79490564efb4fabfdc
Patrick <patoread@gmail.com>
2022-05-09 14:02:42 +0200

    feat: Create navigation bar
    The navigation bar that is going to be used in this project has been created. The styling has been created using Tailwind. The links to the pages have not been inserted because they are still to be created

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1 class="text-red-500">Im a navigation bar!</h1>
+  <nav
+    class="w-full h-14 bg-red-500 flex justify-between items-center p-3 text-white"
+  >
+    <h1 class="text-white text-xl">Book Quotes</h1>
+    <ul class="flex space-x-3">
+      <li
+        class="cursor-pointer hover:outline hover:outline-red-400 p-1 rounded"
+      >
+        <NuxtLink>Quotes</NuxtLink>
+      </li>
+      <li
+        class="cursor-pointer hover:outline hover:outline-red-400 p-1 rounded"
+      >
+        <NuxtLink>About</NuxtLink>
+      </li>
+    </ul>
+  </nav>
 </template>
